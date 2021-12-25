8be71463c67c0d3d56d53231541af1c0bca81676
waleed-cs <waleedalharthi1999@gmail.com>
2021-12-25 23:53:43 +0300

    first commit

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
-const progressBar = document.querySelector("#progressBar");
-const body = document.querySelector("body");
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } });
+//goUp button
 
-const animateProgressBar = () => {
-  let scrollDistance = -body.getBoundingClientRect().top;
-  let progressWidth =
-    (scrollDistance /
-      (body.getBoundingClientRect().height -
-        document.documentElement.clientHeight)) *
-    100;
-  let percentValue = Math.floor(progressWidth);
-  if (percentValue == 99) {
-    percentValue = 100;
+const goUp = document.querySelector(".goUp");
+const aboutMeSection = document.querySelector("#aboutMe");
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 200) {
+    goUp.classList.add("show");
+  } else {
+    goUp.classList.remove("show");
   }
-  progressBar.style.width = percentValue + "%";
-};
 
-window.addEventListener("scroll", animateProgressBar);
+  if (this.scrollY >= 320) {
+    aboutMeSection.classList.add("show");
+  } else {
+    aboutMeSection.classList.remove("show");
+  }
+});
+
+goUp.addEventListener("click", () => {
+  // goUp.style.
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+//slider animation
+tl.to(".slider", { y: "-100%", duration: 1, delay: 0.25 });
+
+//text animation
+tl.to(".hide span", { y: "0%", stagger: 0.25 });
+
+//hand waving animation
+tl.fromTo(
+  "#arm",
+  { rotation: "0deg" },
+  { rotation: "-8.5deg", repeat: -1, yoyo: true, duration: 0.5 }
+);
